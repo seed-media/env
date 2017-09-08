d0af1154870c7735237c8d7c6bb825d0ced42c12
benswinburne <ben.swinburne@gmail.com>
2017-09-08 15:48:27 +0100

    Added standard

--- src/index.js
+++ src/index.js
@@ -1,53 +1,47 @@
 'use strict'
 
-const path = require('path')
 const dotenv = require('dotenv')
 
-class Env {
-  constructor (path) {
-    dotenv.load({
-      silent: process.env.ENV_SILENT || false,
-      path: this.path(path || process.env.ENV_PATH)
-    })
-  }
+dotenv.load({
+  path: process.env.ENV_PATH || '.env'
+})
 
-  path (path) {
-    return this.isEmpty(path) ? '.env' : path;
-  }
+module.exports = {
+  load: function (path) {
+    dotenv.load({ path })
+  },
 
-  isEmpty (value) {
+  isEmpty: function (value) {
     return (value && value.length !== 0)
-  }
+  },
 
-  isTruthy (value) {
+  isTruthy: function (value) {
     return (value === 'true' || value === '1')
-  }
+  },
 
-  isFalsey (value) {
+  isFalsey: function (value) {
     return (value === 'false' || value === '0')
-  }
+  },
 
-  hasValue (x) {
-    return (x !== undefined && x !== null )
-  }
+  hasValue: function (x) {
+    return (x !== undefined && x !== null)
+  },
 
-  boolify (value) {
+  boolify: function (value) {
     return this.isTruthy(value) ? true : this.isFalsey(value) ? false : value
-  }
+  },
 
-  exists (key) {
+  exists: function (key) {
     return this.hasValue(process.env[key])
-  }
+  },
 
-  get (key, dflt) {
+  get: function (key, dflt) {
     return this.boolify(
       process.env[key] || (this.hasValue(dflt) ? dflt : null)
     )
-  }
+  },
 
-  set (key, value) {
+  set: function (key, value) {
     process.env[key] = value
   }
 }
-
-module.exports = Env
